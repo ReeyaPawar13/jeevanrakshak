4c379883a40f7378c844f21312e4cb5f1128184f
Reeya Pawar <reeyapawar13@gmail.com>
2026-04-04 03:14:53 +0530

    Re-upload project

--- public/script.js
+++ public/script.js
@@ -514,7 +514,8 @@ function navigateToHospital(hospitalId) {
         
         // Update map with destination
         const mapFrame = document.getElementById('mapFrame');
-        mapFrame.src = `[google.com](https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83923192776!2d77.06889754725782!3d28.52758200617607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2s${encodeURIComponent(hospital.name)}!5e0!3m2!1sen!2sin!4v1620000000000!5m2!1sen!2sin)`;
+       mapFrame.src =
+`https://maps.google.com/maps?q=${encodeURIComponent(hospital.name)}&output=embed`;
         
         showDriverSection('liveRoute');
     }
